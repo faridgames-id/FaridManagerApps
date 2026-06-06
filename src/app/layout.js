7c2dc527345f5e88f395590eb140ec4f24a2ae92
Farid <farid@example.com>
2026-06-06 12:17:52 +0700

    Inject static viewport meta tag for WebViews

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,9 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id" className={`${inter.variable} ${jakarta.variable}`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </head>
       <body>{children}</body>
     </html>
   );
